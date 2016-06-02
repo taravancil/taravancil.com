6ac0e2b4e8c00a9b536e0c2b1da8cd1255aebb09
Tara <tbvanc@gmail.com>
2016-06-02 07:50:53 -0500

    Add hugo server task to gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var rename       = require('gulp-rename');
 var cssmin       = require('gulp-clean-css');
 var minifyHtml   = require('gulp-htmlmin');
 var notification = require('gulp-notify');
+var exec         = require('child_process').exec;
 
 gulp.task('build-scss', function() {
     // TODO: Add notifications for Sass build failures
@@ -18,12 +19,19 @@ gulp.task('build-scss', function() {
 });
 
 gulp.task('minify-html', function() {
-  return gulp.src('public/**/*.html').pipe(minifyHtml());
+    return gulp.src('public/**/*.html').pipe(minifyHtml());
 });
+
+gulp.task('hugo-server', function() {
+    exec('hugo server', function(stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+  });
+})
 
 gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', ['build-scss']);
-  gulp.watch('public/*.html', ['minify-html']);
+    gulp.watch('src/scss/**/*.scss', ['build-scss']);
+    gulp.watch('public/*.html', ['minify-html']);
 });
 
-gulp.task('default', ['build-scss', 'watch']);
+gulp.task('default', ['build-scss', 'watch', 'hugo-server']);

893ba5db12f79f3789d479e7327335183e241a5e
Tara <tbvanc@gmail.com>
2016-06-02 07:34:17 -0500

    Set up Gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var Promise      = require('es6-promise').Promise;
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename       = require('gulp-rename');
+var cssmin       = require('gulp-clean-css');
+var minifyHtml   = require('gulp-htmlmin');
+var notification = require('gulp-notify');
+
+gulp.task('build-scss', function() {
+    // TODO: Add notifications for Sass build failures
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('static/css'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('public/**/*.html').pipe(minifyHtml());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*.scss', ['build-scss']);
+  gulp.watch('public/*.html', ['minify-html']);
+});
+
+gulp.task('default', ['build-scss', 'watch']);

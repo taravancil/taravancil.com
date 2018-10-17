61f0b399483d866b9a07ef16cf7d64a5d84bb4b6
Tara Vancil <tbvanc@gmail.com>
2018-10-16 19:15:06 -0500

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,78 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var autoprefixer = require('gulp-autoprefixer')
-var rename = require('gulp-rename')
-var cssmin = require('gulp-clean-css')
-var minifyHtml = require('gulp-htmlmin')
-var notification = require('gulp-notify')
-var exec = require('child_process').exec
-var plumber = require('gulp-plumber')
-var uglify = require('gulp-uglify')
-var pump = require('pump')
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var rename = require("gulp-rename");
+var cssmin = require("gulp-clean-css");
+var minifyHtml = require("gulp-htmlmin");
+var notification = require("gulp-notify");
+var exec = require("child_process").exec;
+var plumber = require("gulp-plumber");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
 
-gulp.task('build-scss', function () {
-  var onError = function (err) {
+gulp.task("build-scss", function() {
+  var onError = function(err) {
     notification.onError({
-        title: 'gulp: build-scss',
-        message: '<%= error.message %>',
-    })(err)
-    this.emit('end')
-  }
+      title: "gulp: build-scss",
+      message: "<%= error.message %>"
+    })(err);
+    this.emit("end");
+  };
 
-  return gulp.src('src/scss/style.scss')
-    .pipe(plumber({errorHandler: onError}))
+  gulp
+    .src("src/scss/style.scss")
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('static/css/'))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("static/css/"));
 
-  return gulp.src('src/scss/pages/*.scss')
-    .pipe(plumber({errorHandler: onError}))
+  return gulp
+    .src("src/scss/pages/*.scss")
+    .pipe(plumber({ errorHandler: onError }))
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cssmin())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('static/css/'))
-})
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("static/css/"));
+});
 
-gulp.task('minify-html', function () {
-  return gulp.src('public/**/*.html')
-    .pipe(minifyHtml({collapseWhitespace: true}))
-    .pipe(gulp.dest('public'))
-})
+gulp.task("minify-html", function() {
+  return gulp
+    .src("public/**/*.html")
+    .pipe(minifyHtml({ collapseWhitespace: true }))
+    .pipe(gulp.dest("public"));
+});
 
-gulp.task('build-js', function () {
-  pump([
-    gulp.src('src/js/*'),
-    uglify(),
-    gulp.dest('static/js')
-  ])
-})
+gulp.task("build-js", function() {
+  // pump([gulp.src("src/js/*"), uglify(), gulp.dest("static/js")]);
+  return gulp.src("src/js/*").pipe(gulp.dest("static/js"));
+});
 
-gulp.task('hugo-server', function () {
-  exec('hugo server', function(data, err) {
-    console.log(data)
-    console.log(err)
-  })
-})
+gulp.task("hugo-server", function() {
+  exec("hugo server", function(data, err) {
+    console.log(data);
+    console.log(err);
+  });
+});
 
-gulp.task('hugo-build', function () {
-  exec('hugo', function(data, err) {
+gulp.task("hugo-build", function() {
+  exec("hugo", function(data, err) {
     if (err) {
-      exec('hugo')
+      exec("hugo");
     }
-    console.log(data)
-  })
-})
+    console.log(data);
+  });
+});
 
-gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['build-scss', 'hugo-build'])
-  gulp.watch('src/js/*', ['build-js', 'hugo-build'])
-  gulp.watch('layouts/**/*.html', ['hugo-build'])
-  gulp.watch('content/**/*', ['hugo-build'])
-  gulp.watch('data/**/*.yml', ['hugo-build'])
-})
+gulp.task("watch", function() {
+  gulp.watch("src/scss/**/*.scss", ["build-scss", "hugo-build"]);
+  gulp.watch("src/js/*", ["build-js", "hugo-build"]);
+  gulp.watch("layouts/**/*.html", ["hugo-build"]);
+  gulp.watch("content/**/*", ["hugo-build"]);
+  gulp.watch("data/**/*.yml", ["hugo-build"]);
+});
 
-gulp.task('default', ['build-scss', 'build-js', 'watch', 'hugo-server'])
-gulp.task('build', ['hugo-build'])
+gulp.task("default", ["build-scss", "build-js", "watch", "hugo-server"]);
+gulp.task("build", ["hugo-build"]);
